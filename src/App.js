46dda335e94329b28eed922d61930b89de259452
Ahmed Anber <begnulinux@gmail.com>
2019-09-23 19:15:56 +0200

    remove text from header

--- src/App.js
+++ src/App.js
@@ -9,9 +9,6 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
       </header>
       <Quiz />
     </div>
