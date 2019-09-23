c9ede5144be2c277b8d682671abf842d747af0d8
Ahmed Anber <begnulinux@gmail.com>
2019-09-23 19:10:31 +0200

    Updates

---
+++ precache-manifest.0278003fc001cdbe0233700ca84299be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ce9d98cafd1030314d822251564d583",
+    "url": "/react-game/index.html"
+  },
+  {
+    "revision": "82a4c1b975b67d34a928",
+    "url": "/react-game/static/css/main.31022bf2.chunk.css"
+  },
+  {
+    "revision": "10f07366f7a3395b1190",
+    "url": "/react-game/static/js/2.6d4270fa.chunk.js"
+  },
+  {
+    "revision": "82a4c1b975b67d34a928",
+    "url": "/react-game/static/js/main.1717676c.chunk.js"
+  },
+  {
+    "revision": "f4abe13fae1af131a30f",
+    "url": "/react-game/static/js/runtime~main.067350e5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-game/static/media/logo.5d5d9eef.svg"
+  }
+]);
